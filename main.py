420a1496a0d934068d9140255530fce63891c5e3
MrYarikPasik <68771375+MrYarikPasik@users.noreply.github.com>
2022-08-02 19:48:56 +0200

    Dino-bot
    A bot that takes information from the screen, thanks to which it automates the Dino game.

---
+++ main.py
@@ -0,0 +1,42 @@
+from PIL import ImageGrab, ImageOps
+import pyautogui
+from numpy import *
+import time
+
+
+class DinoBot:
+    def __init__(self, replaybtn, dino):
+        self.replaybtn = replaybtn
+        self.dino = dino
+
+    def restartgame(self):
+        pyautogui.click(self.replaybtn)
+
+    def jump(self):
+        pyautogui.keyDown('space')
+        time.sleep(0.05)
+        pyautogui.keyUp('space')
+
+    def grabimage(self):
+        box = (self.dino[0] + 35, self.dino[1], self.dino[0] + 75, self.dino[1] + 30)
+        image = ImageGrab.grab(box)
+        grayImage = ImageOps.grayscale(image)
+        a = array(grayImage.getcolors())
+        return a.sum()
+
+    def start(self):
+        self.restartgame()
+        while True:
+            if self.grabimage() != 1447:
+                self.jump()
+
+
+def main():
+    bot = DinoBot((480, 474), (250, 476))
+    bot.start()
+    time.sleep(1)
+    bot.jump()
+
+
+if __name__ == '__main__':
+    main()
